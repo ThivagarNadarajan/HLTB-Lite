024e87a4726ac0bd0b7d144760febb4c194f3111
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2021-04-05 16:24:04 -0400

    Add bar chart for basic completion data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css';
-import SearchBar from './components/SearchBar';
-
 import { useEffect, useState } from 'react';
+import { BarChart, XAxis, Tooltip, YAxis, Bar } from 'recharts';
 import { Game, getDefaultGames } from './services/gameService';
+
+import SearchBar from './components/SearchBar';
 
 const App = (): JSX.Element => {
 
@@ -11,7 +12,6 @@ const App = (): JSX.Element => {
 	const fetchDefaultGames = async () => {
 		const result = await getDefaultGames();
 		setGames(result);
-		console.log(games);
 	};
 
 	useEffect(() => {
@@ -22,6 +22,29 @@ const App = (): JSX.Element => {
 		<div className="container">
 			<h1>HLTB Lite</h1>
 			<SearchBar />
+			<div className="chart-container">
+				<BarChart
+					width={500}
+					height={500}
+					margin={{ right: 100 }}
+					data={games.map(game => ({
+						tite: game.name,
+						value: game.gameplayMain
+					}))}
+				>
+					<XAxis
+						dataKey="tite"
+						angle={60}
+						textAnchor="start"
+						height={300}
+						width={300}
+						interval={0}
+					/>
+					<YAxis />
+					<Tooltip />
+					<Bar dataKey="value" fill="black" />
+				</BarChart>
+			</div>
 		</div>
 	);
 };
