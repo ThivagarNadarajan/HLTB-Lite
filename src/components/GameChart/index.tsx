93abbeb86d4ac70ddf5a338f0551b8603bbbecd0
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2021-04-06 20:23:36 -0400

    Add all 3 modes to chart data

--- src/components/GameChart/index.tsx
+++ src/components/GameChart/index.tsx
@@ -4,30 +4,40 @@ import { Game } from '../../services/gameService';
 import { BarChart, XAxis, Tooltip, YAxis, Bar } from 'recharts';
 
 const GameChart: React.FC<{ games: Game[]; setGames: React.Dispatch<React.SetStateAction<Game[]>> }>
-	= ({ games, setGames }): JSX.Element =>
-		<div className="chart-container">
-			<button onClick={() => setGames([])}>Clear Games</button>
-			<BarChart
-				width={500}
-				height={500}
-				margin={{ right: 100 }}
-				data={games.map(game => ({
-					tite: game.name,
-					value: game.gameplayMain
-				}))}
-			>
-				<XAxis
-					dataKey="tite"
-					angle={50}
-					textAnchor="start"
-					height={300}
-					width={300}
-					interval={0}
-				/>
-				<YAxis />
-				<Tooltip />
-				<Bar dataKey="value" fill="black" />
-			</BarChart>
-		</div>;
+	= ({ games, setGames }): JSX.Element => {
+
+		const chartData = games.map(game => ({
+			title: game.name,
+			['Main']: game.gameplayMain,
+			['Extra']: game.gameplayMainExtra,
+			['Complete']: game.gameplayCompletionist
+		}));
+
+		return (
+			<div className="chart-container">
+				<button onClick={() => setGames([])}>Clear Games</button>
+				<BarChart
+					width={500}
+					height={500}
+					margin={{ right: 100 }}
+					data={chartData}
+				>
+					<XAxis
+						dataKey="title"
+						angle={50}
+						textAnchor="start"
+						height={300}
+						width={300}
+						interval={0}
+					/>
+					<YAxis />
+					<Tooltip />
+					<Bar dataKey="Main" fill="black" />
+					<Bar dataKey="Extra" fill="salmon" />
+					<Bar dataKey="Complete" fill="lightblue" />
+				</BarChart>
+			</div>
+		);
+	};
 
 export default GameChart;
