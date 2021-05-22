806c1adeee30e9fc9db79e96beaecad05197e28f
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2021-05-22 18:16:19 -0400

    Add ability to remove games from chart

--- src/components/GameChart/index.tsx
+++ src/components/GameChart/index.tsx
@@ -1,10 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
 import './GameChart.css';
 import { FC, useState, useEffect } from 'react';
 import { Game } from '../../services/gameService';
-import { BarChart, XAxis, Tooltip, YAxis, Bar } from 'recharts';
+import { BarChart, XAxis, Tooltip, YAxis, Bar, Rectangle } from 'recharts';
 import { ResponsiveContainer } from 'recharts';
 import { Icon } from '@iconify/react';
 import closeIcon from '@iconify-icons/ion/close';
+import pencilOutline from '@iconify-icons/ion/pencil-outline';
 
 import Dropdown from './Dropdown';
 
@@ -18,6 +21,7 @@ const GameChart: FC<{ games: Game[]; setGames: React.Dispatch<React.SetStateActi
 	= ({ games, setGames }): JSX.Element => {
 		const [completionTypes, setCompletionTypes] = useState<string[]>(Object.values(CompletionTypes));
 		const [isMobile, setIsMobile] = useState(false);
+		const [editMode, setEditMode] = useState(false);
 
 		useEffect(() => {
 			window.addEventListener('resize', () => {
@@ -37,6 +41,7 @@ const GameChart: FC<{ games: Game[]; setGames: React.Dispatch<React.SetStateActi
 
 		}));
 
+
 		return (
 			<div className="chart-container">
 				<div className="chart-options">
@@ -45,9 +50,14 @@ const GameChart: FC<{ games: Game[]; setGames: React.Dispatch<React.SetStateActi
 						selected={completionTypes}
 						setSelected={setCompletionTypes}
 					/>
-					<button className="clear-chart" onClick={() => setGames([])}>
-						<Icon icon={closeIcon} />
-					</button>
+					<div className="chart-controls">
+						<button className={`chart-control edit-chart ${editMode && 'edit-enabled'}`} onClick={() => setEditMode(!editMode)}>
+							<Icon icon={pencilOutline} />
+						</button>
+						<button className={'chart-control clear-chart'} onClick={() => setGames([])}>
+							<Icon icon={closeIcon} />
+						</button>
+					</div>
 				</div>
 				{
 					completionTypes.length
@@ -69,10 +79,14 @@ const GameChart: FC<{ games: Game[]; setGames: React.Dispatch<React.SetStateActi
 										stroke={'#b4bdc3'}
 									/>
 									<YAxis stroke={'#b4bdc3'} />
-									<Tooltip />
+									<Tooltip
+										cursor={<CustomCursor games={games} setGames={setGames} editMode={editMode} />}
+										content={editMode && CustomTooltip}
+									/>
 									<Bar dataKey={CompletionTypes.Main} fill="#e15127" />
 									<Bar dataKey={CompletionTypes.Extra} fill="#c4b693" />
 									<Bar dataKey={CompletionTypes.Complete} fill="#1e5982" />
+
 								</BarChart>
 							</ResponsiveContainer>
 						</div>
@@ -82,5 +96,27 @@ const GameChart: FC<{ games: Game[]; setGames: React.Dispatch<React.SetStateActi
 			</div>
 		);
 	};
+
+
+const CustomCursor = (props) => {
+	const { x, y, width, height, payload, games, setGames, editMode } = props;
+	return <Rectangle
+		strokeWidth="2px"
+		fill={editMode ? '#fa9696' : '#bde0ff'}
+		x={x} y={y}
+		width={width}
+		height={height}
+		onClick={
+			editMode
+				? (() => setGames(games.filter(game => game.name != payload[0].payload.title)))
+				: () => null
+		}
+		style={editMode ? { cursor: 'pointer' } : {}}
+	/>;
+};
+
+const CustomTooltip = () => {
+	return <div className="removal-tooltip">Remove game from chart</div>;
+};
 
 export default GameChart;
